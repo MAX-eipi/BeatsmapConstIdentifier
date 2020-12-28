122adc1dbf50a2c162d448b00686c145df8ac16a
MAX-eipi <max.eipi.3141@gmail.com>
2020-12-28 18:44:04 +0900

    code format

--- BeatsmapConstIdentifier.cpp
+++ BeatsmapConstIdentifier.cpp
@@ -14,11 +14,11 @@
 using namespace std;
 
 // 十分大きな値として inf を定義
-const int inf=1000000007;
+const int inf = 1000000007;
 
 // ConstIneq[ 曲ID ] : pair<int,int> の vector
 // .first <= (曲IDの譜面定数) <= .second
-vector<pair<int,int>> ConstIneq;
+vector<pair<int, int>> ConstIneq;
 
 // RelateSong[ 曲ID ] : set<int> の vector
 // RelateSong[ i ] の要素 = (曲ID i)と何らかの関係式が存在する曲IDの集合
@@ -28,21 +28,21 @@ vector<set<int>> RelateSong;
 // keyが pair<int,int> であり value が int である map
 // 「 (tの譜面定数) >= (sの譜面定数) + v 」(vが負になる可能性もある)
 // 最も緩い制約は、v=-inf
-map<pair<int,int>,int> ReqMin;
+map<pair<int, int>, int> ReqMin;
 
 // ReqMax[ (曲ID : s , 曲ID : t) ] = {数値v} の、
 // keyが pair<int,int> であり value が int である map
 // 「 (tの譜面定数) <= (sの譜面定数) + v 」(vが負になる可能性もある)
 // 最も緩い制約は、v=inf
-map<pair<int,int>,int> ReqMax;
+map<pair<int, int>, int> ReqMax;
 
 // 総曲数(曲IDの最大値)を取得
 // もし4難易度全てについて調べるなら、収録曲数の4倍必要
-int GetSongNum(){
-  int res;
-  // 標準入力から入力する
-  cin >> res;
-  return res;
+int GetSongNum() {
+    int res;
+    // 標準入力から入力する
+    cin >> res;
+    return res;
 }
 
 // 譜面定数が小数第1位までしか持たないことを前提にして、
@@ -54,42 +54,42 @@ int GetSongNum(){
 // -> 矛盾しているので、エラーを送出
 // 返り値 : bool
 // true - 異常なし , false - 矛盾が発生
-bool ConstValidate(int &cfir,int &csec){
-  // 10単位(本来の0.1単位)に丸める
-  if(cfir%10 != 0){cfir += (10 - cfir%10);}
-  csec -= (csec%10);
+bool ConstValidate(int& cfir, int& csec) {
+    // 10単位(本来の0.1単位)に丸める
+    if (cfir % 10 != 0) { cfir += (10 - cfir % 10); }
+    csec -= (csec % 10);
 
-  if(cfir > csec){return false;}
-  return true;
+    if (cfir > csec) { return false; }
+    return true;
 }
 
 // [曲ID] の譜面定数が first 以上 second 以下であるとの情報を ConstIneq に追加
 // (ConstValidateを含む)
 // 返り値 : bool
 // true - 異常なし , false - 矛盾が発生
-bool ConstUpdate(int id,int first,int second){
-  int cfir=max(ConstIneq[id].first,first);
-  int csec=min(ConstIneq[id].second,second);
+bool ConstUpdate(int id, int first, int second) {
+    int cfir = max(ConstIneq[id].first, first);
+    int csec = min(ConstIneq[id].second, second);
 
-  if(!ConstValidate(cfir,csec)){return false;}
+    if (!ConstValidate(cfir, csec)) { return false; }
 
-  ConstIneq[id].first=cfir;
-  ConstIneq[id].second=csec;
-  return true;
+    ConstIneq[id].first = cfir;
+    ConstIneq[id].second = csec;
+    return true;
 }
 
 // 曲IDがid番の曲について、筐体表示レベルで定数を初期化
-void InitSongLevel(int id){
-  int fir,sec;
-  // 標準入力から入力する
-  // ここで、譜面定数は100倍した形で使用することに注意
-  // 12の場合、 fir=1200 sec=1260
-  // 13+の場合、 fir=1370 sec=1390
-  // 無効な曲IDであれば、 fir=-1 sec=-2 など無効なデータで初期化
-  cin >> fir >> sec;
-  ConstIneq[id].first=fir;
-  ConstIneq[id].second=sec;
-  return;
+void InitSongLevel(int id) {
+    int fir, sec;
+    // 標準入力から入力する
+    // ここで、譜面定数は100倍した形で使用することに注意
+    // 12の場合、 fir=1200 sec=1260
+    // 13+の場合、 fir=1370 sec=1390
+    // 無効な曲IDであれば、 fir=-1 sec=-2 など無効なデータで初期化
+    cin >> fir >> sec;
+    ConstIneq[id].first = fir;
+    ConstIneq[id].second = sec;
+    return;
 }
 
 // スコアを (定数) + x という形に変換、xを返す
@@ -102,21 +102,21 @@ void InitSongLevel(int id){
 // 定数用基準スコア
 // BaseScore[i]であれば丁度BaseOffset[i] 加点される
 // 間は線形補間
-const int BaseElement=5;
-const vector<int> BaseScore={1007500,1000000,975000,925000,900000};
-const vector<int> BaseOffset={200,100,0,-300,-500};
+const int BaseElement = 5;
+const vector<int> BaseScore = { 1007500,1000000,975000,925000,900000 };
+const vector<int> BaseOffset = { 200,100,0,-300,-500 };
 
-int ScoreToOffset(int score){
-  if(score>=BaseScore[0]){return BaseOffset[0];}
-  for(int i=1;i<BaseElement;i++){
-    if(score>=BaseScore[i]){
-      // 小数第3位より下は自動切り捨て
-      // 計算順に注意! C++の int(32bit符号付き整数) には収まる
-      return BaseOffset[i] + ((BaseOffset[i-1]-BaseOffset[i])*(score-BaseScore[i]))/(BaseScore[i-1]-BaseScore[i]);
+int ScoreToOffset(int score) {
+    if (score >= BaseScore[0]) { return BaseOffset[0]; }
+    for (int i = 1; i < BaseElement; i++) {
+        if (score >= BaseScore[i]) {
+            // 小数第3位より下は自動切り捨て
+            // 計算順に注意! C++の int(32bit符号付き整数) には収まる
+            return BaseOffset[i] + ((BaseOffset[i - 1] - BaseOffset[i]) * (score - BaseScore[i])) / (BaseScore[i - 1] - BaseScore[i]);
+        }
     }
-  }
-  // A未満なので、データ破棄
-  return -inf;
+    // A未満なので、データ破棄
+    return -inf;
 }
 
 // 追加した情報を処理
@@ -124,146 +124,146 @@ int ScoreToOffset(int score){
 // 下のInputSetData内で呼ばれる
 // 返り値 : bool
 // true : 成功 , false : データ破損
-bool Run(queue<int> &qu){
-  while(!qu.empty()){
-    int cid=qu.front(); // 着目する曲ID
-    qu.pop(); // queueの先頭削除
-    for(auto nid : RelateSong[cid]){ // nid を cid と関連する曲ID全てについて走査
-      bool change=false; // nidに関する制約に変動があったかどうか
-      pair<int,int> CNp=make_pair(cid,nid);
-      // 最大値規定制約
-      if(ConstIneq[cid].second+ReqMax[CNp] < ConstIneq[nid].second){change=true;}
-      // 最小値規定制約
-      if(ConstIneq[cid].first+ReqMin[CNp] > ConstIneq[nid].first){change=true;}
+bool Run(queue<int>& qu) {
+    while (!qu.empty()) {
+        int cid = qu.front(); // 着目する曲ID
+        qu.pop(); // queueの先頭削除
+        for (auto nid : RelateSong[cid]) { // nid を cid と関連する曲ID全てについて走査
+            bool change = false; // nidに関する制約に変動があったかどうか
+            pair<int, int> CNp = make_pair(cid, nid);
+            // 最大値規定制約
+            if (ConstIneq[cid].second + ReqMax[CNp] < ConstIneq[nid].second) { change = true; }
+            // 最小値規定制約
+            if (ConstIneq[cid].first + ReqMin[CNp] > ConstIneq[nid].first) { change = true; }
 
-      // ConstIneq[nid] に変動が生じる場合
-      if(change){
-        // 新たな制約を追加して壊れたら、破滅
-        if(!ConstUpdate(nid,ConstIneq[cid].first+ReqMin[CNp],ConstIneq[cid].second+ReqMax[CNp])){
-          return false;
+            // ConstIneq[nid] に変動が生じる場合
+            if (change) {
+                // 新たな制約を追加して壊れたら、破滅
+                if (!ConstUpdate(nid, ConstIneq[cid].first + ReqMin[CNp], ConstIneq[cid].second + ReqMax[CNp])) {
+                    return false;
+                }
+                // nid を基準とする処理を処理待ち queue に追加
+                qu.push(nid);
+            }
         }
-        // nid を基準とする処理を処理待ち queue に追加
-        qu.push(nid);
-      }
     }
-  }
-  return true;
+    return true;
 }
 
 // Best枠 (Recent枠) 情報入力
 // 返り値 : bool
 // true : 成功 , false : データ破損
-bool InputSetData(){
-  // 入力は全て標準入力から行っている
+bool InputSetData() {
+    // 入力は全て標準入力から行っている
 
-  int SetSong;
-  cin >> SetSong; // 何曲からなるデータか? (Bestなら30、Recentなら10)
-  vector<int> Songid,Offset; // 初めは空
-  for(int i=0;i<SetSong;i++){
-    int inid,insc;
-    cin >> inid >> insc;
-    insc=ScoreToOffset(insc); // スコアをオフセットに変換
-    if(insc<0){continue;} // Sに満たない、オフセット 0 未満ならデータを破棄
-    Songid.push_back(inid);
-    Offset.push_back(insc);
-  }
-  SetSong=Songid.size(); // 有効なデータが何曲あるかに更新
+    int SetSong;
+    cin >> SetSong; // 何曲からなるデータか? (Bestなら30、Recentなら10)
+    vector<int> Songid, Offset; // 初めは空
+    for (int i = 0; i < SetSong; i++) {
+        int inid, insc;
+        cin >> inid >> insc;
+        insc = ScoreToOffset(insc); // スコアをオフセットに変換
+        if (insc < 0) { continue; } // Sに満たない、オフセット 0 未満ならデータを破棄
+        Songid.push_back(inid);
+        Offset.push_back(insc);
+    }
+    SetSong = Songid.size(); // 有効なデータが何曲あるかに更新
 
-  // i-1個目とi個目のデータについて、データを追加
-  for(int i=1;i<SetSong;i++){
-    int Uid=Songid[i-1],Uofs=Offset[i-1]; // 順位が高い方
-    int Lid=Songid[i],Lofs=Offset[i]; // 順位が低い方
-    if(Uid==Lid){continue;} // 同じ曲同士なら、処理を無視
-    pair<int,int> ULp=make_pair(Uid,Lid);
-    pair<int,int> LUp=make_pair(Lid,Uid);
+    // i-1個目とi個目のデータについて、データを追加
+    for (int i = 1; i < SetSong; i++) {
+        int Uid = Songid[i - 1], Uofs = Offset[i - 1]; // 順位が高い方
+        int Lid = Songid[i], Lofs = Offset[i]; // 順位が低い方
+        if (Uid == Lid) { continue; } // 同じ曲同士なら、処理を無視
+        pair<int, int> ULp = make_pair(Uid, Lid);
+        pair<int, int> LUp = make_pair(Lid, Uid);
 
-    //はじめての関係なら、その関係の初期化処理を行う
-    if(RelateSong[Uid].find(Lid) == RelateSong[Uid].end()){
-      //関係を追加
-      RelateSong[Uid].insert(Lid);
-      RelateSong[Lid].insert(Uid);
-      //最も甘い条件で初期化
-      ReqMin[ULp]=-inf;
-      ReqMax[ULp]=inf;
-      ReqMin[LUp]=-inf;
-      ReqMax[LUp]=inf;
+        //はじめての関係なら、その関係の初期化処理を行う
+        if (RelateSong[Uid].find(Lid) == RelateSong[Uid].end()) {
+            //関係を追加
+            RelateSong[Uid].insert(Lid);
+            RelateSong[Lid].insert(Uid);
+            //最も甘い条件で初期化
+            ReqMin[ULp] = -inf;
+            ReqMax[ULp] = inf;
+            ReqMin[LUp] = -inf;
+            ReqMax[LUp] = inf;
+        }
+
+        // (上の定数) + Uofs >= (下の定数) + Lofs
+        // 上を基準に、下の最大値制約が規定、必要があれば更新される
+        ReqMax[ULp] = min(Uofs - Lofs, ReqMax[ULp]);
+        // 下を基準に、上の最小値制約が規定、必要があれば更新される
+        ReqMin[LUp] = max(Lofs - Uofs, ReqMin[LUp]);
+        // ここで、minとmaxが逆に対応することに注意(これで正しいです)
     }
 
-    // (上の定数) + Uofs >= (下の定数) + Lofs
-    // 上を基準に、下の最大値制約が規定、必要があれば更新される
-    ReqMax[ULp]=min(Uofs-Lofs,ReqMax[ULp]);
-    // 下を基準に、上の最小値制約が規定、必要があれば更新される
-    ReqMin[LUp]=max(Lofs-Uofs,ReqMin[LUp]);
-    // ここで、minとmaxが逆に対応することに注意(これで正しいです)
-  }
-
-  queue<int> qu; // Runに渡すqueue
-  for(int i=0;i<SetSong;i++){qu.push(Songid[i]);} // 関係が動きうる全曲を追加
-  return Run(qu); // 影響を伝播
+    queue<int> qu; // Runに渡すqueue
+    for (int i = 0; i < SetSong; i++) { qu.push(Songid[i]); } // 関係が動きうる全曲を追加
+    return Run(qu); // 影響を伝播
 }
 
 // 単曲制約追加
 // 返り値 : bool
 // true : 成功 , false : データ破損
-bool InputOneData(){
-  // 入力は全て標準入力から行っている
-  int id,first,second;
-  cin >> id >> first >> second;
-  if(!ConstUpdate(id,first,second)){return false;} // 単曲で定数制約更新
-  queue<int> qu;
-  qu.push(id);
-  return Run(qu); // 影響を伝播
+bool InputOneData() {
+    // 入力は全て標準入力から行っている
+    int id, first, second;
+    cin >> id >> first >> second;
+    if (!ConstUpdate(id, first, second)) { return false; } // 単曲で定数制約更新
+    queue<int> qu;
+    qu.push(id);
+    return Run(qu); // 影響を伝播
 }
 
-int main(){
-  // 実運用の場合、初期化を行うのは最初の1回だけでよい
-  int SongNum = GetSongNum(); // 総曲数(曲IDの最大値)の取得
-  ConstIneq.resize(SongNum+1);
-  RelateSong.resize(SongNum+1);
-  ConstIneq[0]=make_pair(-1,-2); // 曲ID0番に、無効なデータを番兵として置く
-  for(int i=1;i<=SongNum;i++){
-    InitSongLevel(i); // 曲IDがi番の曲について、筐体表示レベルを入力
-  }
+int main() {
+    // 実運用の場合、初期化を行うのは最初の1回だけでよい
+    int SongNum = GetSongNum(); // 総曲数(曲IDの最大値)の取得
+    ConstIneq.resize(SongNum + 1);
+    RelateSong.resize(SongNum + 1);
+    ConstIneq[0] = make_pair(-1, -2); // 曲ID0番に、無効なデータを番兵として置く
+    for (int i = 1; i <= SongNum; i++) {
+        InitSongLevel(i); // 曲IDがi番の曲について、筐体表示レベルを入力
+    }
 
-  // Best枠 (Recent枠) 情報入力ゾーン
-  // 旧バージョンの状態の各枠を間違って取り込まないよう、注意!
-  // (直近プレイ記録の最新プレイ時刻とかで判定してください)
-  while(1){
-    // 標準入力から Set/One/No 入力 (Case-sensitive)
-    // "Set" ... 枠の情報を入力する
-    // "One" ... ある曲の定数の情報を入力する
-    // "No" ... 終わる
-    string s;
-    while(1){
-      cin >> s;
-      if(s=="Set" || s=="One" || s=="No"){break;}
-      cout << "Error : Input should be Set / One / No (Case-sensitive)\n";
+    // Best枠 (Recent枠) 情報入力ゾーン
+    // 旧バージョンの状態の各枠を間違って取り込まないよう、注意!
+    // (直近プレイ記録の最新プレイ時刻とかで判定してください)
+    while (1) {
+        // 標準入力から Set/One/No 入力 (Case-sensitive)
+        // "Set" ... 枠の情報を入力する
+        // "One" ... ある曲の定数の情報を入力する
+        // "No" ... 終わる
+        string s;
+        while (1) {
+            cin >> s;
+            if (s == "Set" || s == "One" || s == "No") { break; }
+            cout << "Error : Input should be Set / One / No (Case-sensitive)\n";
+        }
+        if (s == "No") { break; }
+        // Best枠かRecent枠のデータを、1個分取得する
+        if (s == "Set") {
+            if (!InputSetData()) {
+                // データがどこかで破損している
+                cout << "Error : Crashed!!\n";
+                return 0;
+            }
+        }
+        // ある曲について、制約を追加
+        if (s == "One") {
+            if (!InputOneData()) {
+                // データがどこかで破損している
+                cout << "Error : Crashed!!\n";
+                return 0;
+            }
+        }
     }
-    if(s=="No"){break;}
-    // Best枠かRecent枠のデータを、1個分取得する
-    if(s=="Set"){
-      if(!InputSetData()){
-        // データがどこかで破損している
-        cout << "Error : Crashed!!\n";
-        return 0;
-      }
-    }
-    // ある曲について、制約を追加
-    if(s=="One"){
-      if(!InputOneData()){
-        // データがどこかで破損している
-        cout << "Error : Crashed!!\n";
-        return 0;
-      }
-    }
-  }
 
-  // 終了処理
-  for(int i=1;i<=SongNum;i++){
-    // 最終結果の出力
-    cout << i << ":[" << ConstIneq[i].first << "," << ConstIneq[i].second << "]\n";
-  }
-  return 0;
+    // 終了処理
+    for (int i = 1; i <= SongNum; i++) {
+        // 最終結果の出力
+        cout << i << ":[" << ConstIneq[i].first << "," << ConstIneq[i].second << "]\n";
+    }
+    return 0;
 }
 
 // 以下、テストケース入力形式のメモ
